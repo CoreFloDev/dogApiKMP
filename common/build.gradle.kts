72ab9bb1aa5312ff3298b2cfd27d8380b05c94c3
CoreFloDev <CoreFloDev@users.noreply.github.com>
2023-12-31 14:27:02 +0100

    Have common module linking with ios working

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "common"
+            baseName = "Common"
             isStatic = true
         }
     }
--- feature/details/build.gradle.kts
+++ feature/details/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "featureDetails"
+            baseName = "FeatureDetails"
             isStatic = true
         }
     }
