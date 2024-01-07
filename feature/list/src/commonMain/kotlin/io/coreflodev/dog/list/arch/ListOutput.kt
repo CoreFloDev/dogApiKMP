302856789aa261d7bc868a898ff0b92da072a700
CoreFloDev <CoreFloDev@users.noreply.github.com>
2024-01-07 12:26:18 +0100

    add list of dogs

--- feature/list/src/commonMain/kotlin/io/coreflodev/dog/list/arch/ListOutput.kt
+++ feature/list/src/commonMain/kotlin/io/coreflodev/dog/list/arch/ListOutput.kt
@@ -2,10 +2,10 @@ package io.coreflodev.dog.list.arch
 
 import io.coreflodev.common.arch.ScreenOutput
 
-sealed class ListOutput : ScreenOutput {
-    data object Loading : ListOutput()
-    data object Retry : ListOutput()
-    data class Display(val list: List<UiDog>) : ListOutput()
+sealed interface ListOutput : ScreenOutput {
+    data object Loading : ListOutput
+    data object Retry : ListOutput
+    data class Display(val list: List<UiDog>) : ListOutput
 }
 
 data class UiDog(val id: String, val image: String, val name: String)
--- iosApp/iosApp/ListView.swift
+++ iosApp/iosApp/ListView.swift
@@ -1,22 +1,69 @@
 import SwiftUI
 import Shared
+import Combine
 
 struct ListView: View {
-    @State private var showContent = false
-    
-    // @StateObject
-    private static var screen = InjectListComponent(appComponent: IOSAppComponent.app)
+    @ObservedObject private var vm = ListViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
-            
+                switch onEnum(of: vm.content) {
+                case .loading:
+                    Text("loadiing")
+                case .retry:
+                    Text("Retry")
+                case .display(let res):
+                    List {
+                        ForEach(res.list, id: \.id) { item in
+                            VStack {
+                                Text(item.name)
+                                AsyncImage(url: URL(string: item.image))
+                            }
+                        }
+                    }
+                case nil:
+                    Text("nulle")
+                }
             }
             .navigationTitle("Dog list")
         }
+        .onAppear{
+            Task {
+                await vm.onAppear()
+            }
+        }
+        .onDisappear(perform: {
+            vm.onDisappear()
+        })
     }
 }
 
 #Preview {
     ListView()
+}
+
+class ListViewModel: ObservableObject {
+    private let screen = InjectListComponent(appComponent: IOSAppComponent.app).screen
+
+    @Published var content: ListOutput? = nil
+
+    func onAppear() async {
+        let attach = screen.attach()
+        let nav = attach.navigation
+        let output = attach.output
+        let input = attach.input
+
+        for await aout in output {
+            content = aout
+        }
+    }
+
+    func onDisappear() {
+        screen.detach()
+    }
+
+    deinit {
+        screen.terminate()
+    }
 }
